f36b048c050bff9c268d1b837c239b866b78a0b8
Prasanjeevi <ps@Prasanjeevis-MacBook-Pro.local>
2018-12-11 14:29:02 +0530

    modified artifact path in build.sh

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -3,19 +3,15 @@
 cd resource-git
 cd product-catalog
 gradle clean assemble
-mkdir ../artifacts
-cp build/libs/*.jar ../artifacts/
+cp build/libs/*.jar ../../artifacts/
 cd ..
 
 cd order
 gradle clean assemble
-cp build/libs/*.jar ../artifacts/
+cp build/libs/*.jar ../../artifacts/
 cd ..
 
 cd frontend
 gradle clean assemble
-cp build/libs/*.jar ../artifacts/
-ls ../artifacts
-cd ..
-pwd
-ls
+cp build/libs/*.jar ../../artifacts/
+ls ../../artifacts
